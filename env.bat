01c4ad0502757ae2eb42131b0327e4d264ffbaf7
B, Prashanth Natraj <prashanth.natraj.b@intel.com>
2021-11-19 09:52:41 +0530

    Test
    Signed-off-by: B, Prashanth Natraj <prashanth.natraj.b@intel.com>

---
+++ env.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+echo %LOCAL_WORKSPACE% 
+echo %MAJOR_VERSION%
+echo %MINOR_VERSION%
+echo %RELEASE_VERSION%
+echo %BUILD_ID% 
+echo %RELEASE_ID%
+echo %BRANCH% 
+echo %RELEASE_DIR%
+echo %RELEASE_SDK_PATH%
